6daedeb79a6cf35ea3eaf2aa0c44999e38dd327d
Tommy Keswick <tkeswick@chillco.com>
2014-10-24 20:18:49 -0700

    add template_preprocess_page function and edit dynamic sidebars section

--- template.php
+++ template.php
@@ -1,6 +1,204 @@
 <?php
 
 /**
+ * Implements template_preprocess_page
+ *
+ */
+function foundation_librarydams_preprocess_page(&$variables) {
+  // Add page--node_type.tpl.php suggestions
+  if (!empty($variables['node'])) {
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+
+  $variables['logo_img'] = '';
+  if (!empty($variables['logo'])) {
+    $variables['logo_img'] = theme('image', array(
+      'path'  => $variables['logo'],
+      'alt'   => strip_tags($variables['site_name']) . ' ' . t('logo'),
+      'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      'attributes' => array(
+        'class' => array('logo'),
+      ),
+    ));
+  }
+
+  $variables['linked_logo']  = '';
+  if (!empty($variables['logo_img'])) {
+    $variables['linked_logo'] = l($variables['logo_img'], '<front>', array(
+      'attributes' => array(
+        'rel'   => 'home',
+        'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      ),
+      'html' => TRUE,
+    ));
+  }
+
+  $variables['linked_site_name'] = '';
+  if (!empty($variables['site_name'])) {
+    $variables['linked_site_name'] = l($variables['site_name'], '<front>', array(
+      'attributes' => array(
+        'rel'   => 'home',
+        'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      ),
+    ));
+  }
+
+  // Top bar.
+  if ($variables['top_bar'] = theme_get_setting('zurb_foundation_top_bar_enable')) {
+    $top_bar_classes = array();
+
+    if (theme_get_setting('zurb_foundation_top_bar_grid')) {
+      $top_bar_classes[] = 'contain-to-grid';
+    }
+
+    if (theme_get_setting('zurb_foundation_top_bar_sticky')) {
+      $top_bar_classes[] = 'sticky';
+    }
+
+    if ($variables['top_bar'] == 2) {
+      $top_bar_classes[] = 'show-for-small';
+    }
+
+    $variables['top_bar_classes'] = implode(' ', $top_bar_classes);
+    $variables['top_bar_menu_text'] = check_plain(theme_get_setting('zurb_foundation_top_bar_menu_text'));
+
+    $top_bar_options = array();
+
+    if (!theme_get_setting('zurb_foundation_top_bar_custom_back_text')) {
+      $top_bar_options[] = 'custom_back_text:false';
+    }
+
+    if ($back_text = check_plain(theme_get_setting('zurb_foundation_top_bar_back_text'))) {
+      if ($back_text !== 'Back') {
+        $top_bar_options[] = "back_text:'{$back_text}'";
+      }
+    }
+
+    if (!theme_get_setting('zurb_foundation_top_bar_is_hover')) {
+      $top_bar_options[] = 'is_hover:false';
+    }
+
+    if (!theme_get_setting('zurb_foundation_top_bar_scrolltop')) {
+      $top_bar_options[] = 'scrolltop:false';
+    }
+
+    $variables['top_bar_options'] = ' data-options="' . implode('; ', $top_bar_options) . '"';
+  }
+
+  // Alternative header.
+  // This is what will show up if the top bar is disabled or enabled only for
+  // mobile.
+  if ($variables['alt_header'] = ($variables['top_bar'] != 1)) {
+    // Hide alt header on mobile if using top bar in mobile.
+    $variables['alt_header_classes'] = $variables['top_bar'] == 2 ? ' hide-for-small' : '';
+  }
+
+  // Menus for alternative header.
+  $variables['alt_main_menu'] = '';
+
+  if (!empty($variables['main_menu'])) {
+    $variables['alt_main_menu'] = theme('links__system_main_menu', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'id' => 'main-menu-links',
+        'class' => array('links', 'inline-list', 'clearfix'),
+      ),
+      'heading' => array(
+        'text' => t('Main menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  $variables['alt_secondary_menu'] = '';
+
+  if (!empty($variables['secondary_menu'])) {
+    $variables['alt_secondary_menu'] = theme('links__system_secondary_menu', array(
+      'links' => $variables['secondary_menu'],
+      'attributes' => array(
+        'id' => 'secondary-menu-links',
+        'class' => array('links', 'clearfix'),
+      ),
+      'heading' => array(
+        'text' => t('Secondary menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  // Top bar menus.
+  $variables['top_bar_main_menu'] = '';
+  if (!empty($variables['main_menu'])) {
+    $variables['top_bar_main_menu'] = theme('links__topbar_main_menu', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'id' => 'main-menu',
+        'class' => array('main-nav'),
+      ),
+      'heading' => array(
+        'text' => t('Main menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  $variables['top_bar_secondary_menu'] = '';
+  if (!empty($variables['secondary_menu'])) {
+    $variables['top_bar_secondary_menu'] = theme('links__topbar_secondary_menu', array(
+      'links' => $variables['secondary_menu'],
+      'attributes' => array(
+        'id'    => 'secondary-menu',
+        'class' => array('secondary', 'link-list'),
+      ),
+      'heading' => array(
+        'text' => t('Secondary menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  // Messages in modal.
+  $variables['zurb_foundation_messages_modal'] = theme_get_setting('zurb_foundation_messages_modal');
+
+  // Convenience variables
+  if (!empty($variables['page']['sidebar_first'])){
+    $left = $variables['page']['sidebar_first'];
+  }
+
+  if (!empty($variables['page']['sidebar_second'])) {
+    $right = $variables['page']['sidebar_second'];
+  }
+
+  // Dynamic sidebars
+  if (!empty($left) && !empty($right)) {
+    $variables['main_grid'] = 'large-4 push-4';
+    $variables['sidebar_first_grid'] = 'large-4 pull-4';
+    $variables['sidebar_sec_grid'] = 'large-4';
+  } elseif (empty($left) && !empty($right)) {
+    $variables['main_grid'] = 'large-8';
+    $variables['sidebar_first_grid'] = '';
+    $variables['sidebar_sec_grid'] = 'large-4';
+  } elseif (!empty($left) && empty($right)) {
+    $variables['main_grid'] = 'large-8 push-4';
+    $variables['sidebar_first_grid'] = 'large-4 pull-8';
+    $variables['sidebar_sec_grid'] = '';
+  } else {
+    $variables['main_grid'] = 'large-12';
+    $variables['sidebar_first_grid'] = '';
+    $variables['sidebar_sec_grid'] = '';
+  }
+
+  // Ensure modal reveal behavior if modal messages are enabled.
+  if(theme_get_setting('zurb_foundation_messages_modal')) {
+    drupal_add_js(drupal_get_path('theme', 'zurb_foundation') . '/js/behavior/reveal.js');
+  }
+}
+
+/**
  * Implements hook_preprocess_block()
  */
 function foundation_librarydams_preprocess_block(&$variables) {
